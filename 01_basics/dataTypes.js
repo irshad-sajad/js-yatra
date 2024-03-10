2ffcd3a322eda5d4440886a47e55815c2eca6e32
Irshad Sajad <111044621+irshad-sajad@users.noreply.github.com>
2024-03-10 17:20:23 +0000

    datatypes

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -9,14 +9,14 @@ let age = 22;
 let isLoggedIn = false;
 let state; //undefined
 
-dataTypes:
-number => 2 to power 53 (2^53)
-bigInt => introduced by ES2020 to store large integers
-string => "sequence/assembly of characters"
-boolean => true or false
-null => standalone value (type = object)
-undefined => place holder for a value which is not defined yet (type = undefined)
-symbol => introduced by ES6 for unique identifiers
+// dataTypes:
+// number => 2 to power 53 (2^53)
+// bigInt => introduced by ES2020 to store large integers
+// string => "sequence/assembly of characters"
+// boolean => true or false
+// null => standalone value (type = object)
+// undefined => place holder for a value which is not defined yet (type = undefined)
+// symbol => introduced by ES6 for unique identifiers
 
 
 console.log(typeof "irshad"); //string
