7778a6615dcfd2c0823a5ef4dc08f6fb73446f65
Irshad Sajad <111044621+irshad-sajad@users.noreply.github.com>
2024-03-10 17:48:55 +0000

    conversion of datatypes

---
+++ 01_basics/typeConversion.js
@@ -0,0 +1,28 @@
+let score = true;
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (type = number)
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "irshad" => true 
+
+let number = 33;
+
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
