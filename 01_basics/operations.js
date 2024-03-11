0fb6833cd0a60e1e6ba29b89a8c8bcc6db5a6f67
Irshad Sajad <111044621+irshad-sajad@users.noreply.github.com>
2024-03-11 13:45:46 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,38 @@
+let value = 3;
+let negValue = -value;
+//console.log(negValue); //-3
+
+//Arithmetic Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2); //2^2
+// console.log(2%2); //remainder
+
+let str1 = "hello ";
+let str2 = "irshad";
+
+let str3 = str1 + str2; //concatenation
+//console.log(str3);
+
+//number conversion to string
+//not allowed, not a good way
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log((3+4)*5%3); //better way
+
+// console.log(+true); //1
+// console.log(true+); //error
+// console.log(+""); //1
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2; //not readable
+
+let gameCounter = 100;
+gameCounter++; //postfix 
+console.log(gameCounter); //101
