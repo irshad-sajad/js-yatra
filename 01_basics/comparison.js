c812bb68f3f80ac7b6c359da4817ce92471981df
Irshad Sajad <111044621+irshad-sajad@users.noreply.github.com>
2024-03-11 14:12:00 +0000

    comparisons of dataTypes

---
+++ 01_basics/comparison.js
@@ -0,0 +1,25 @@
+//Comparing same dataTypes
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+//Comparing different dataTypes
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+
+
+//Avoid such kind of comparisons
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+
+
+// strict equality(===) => checks both value & dataType
+console.log("2" === 2); //false
